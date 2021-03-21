0d5a52979453201bc30c198708c5fe647ff6b1a0
camila <camilasangobar@gmail.com>
2021-03-21 20:37:45 -0300

    graus

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace graus
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.DarkMagenta;
+            Console.WriteLine("----GrausFC----");
+            Console.ResetColor();
+
+            Console.WriteLine();
+            Console.WriteLine("digite:");
+            double f = Convert.ToDouble(Console.ReadLine());
+            
+            Console.WriteLine();
+            double c = (f - 32) / 1.8;
+            Console.WriteLine($"{f} F equivalem a {c} C ");
+
+
+        }
+    }
+}
